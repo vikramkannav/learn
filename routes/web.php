5142dd0bb4ef676104d90fe0c8dfebcf0b5ea2ab
Vikram Sharma <vikram@headerlabs.com>
2017-09-14 13:50:55 +0530

    auth-login with auth module

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+//=======Add after auth============================
+Route::get('/home', 'HomeController@index')->name('home');
